da14d4d67659b0b8551c8fe42ebd378b482c54fd
뚜비 <63052097+juijeong8324@users.noreply.github.com>
2021-09-19 16:57:15 +0900

    Update main.py

--- 1271번[엄청난부자2]/main.py
+++ 1271번[엄청난부자2]/main.py
@@ -1,4 +1,4 @@
-#Link : https://www.acmicpc.net/problem/1271
+# [문제로가기](https://www.acmicpc.net/problem/1271)
 # n과 m의 범위가 너무 커서 파이썬으로 해결.. c++ 로 해결할 시 브론즈 노노..
 
 a, b = map(int, input().split()) # 문자열로 받고 int로 변환!! 
