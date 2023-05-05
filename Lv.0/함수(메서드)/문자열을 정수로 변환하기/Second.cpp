fe27bf24213d20e4537603ed21c70422ffdf77b3
Justice <63052097+juijeong8324@users.noreply.github.com>
2023-05-05 15:49:49 +0900

    Create Second.cpp

---
+++ Lv.0/함수(메서드)/문자열을 정수로 변환하기/Second.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string n_str) {
+    int answer = 0;
+    
+    for(int i=0;i<n_str.size();i++){
+        answer *= 10; // 자릿수를 결정, 제일 높은 자릿수는 10^(n-1)이고, 가장 낮은 자릿수는 10^0이다. 
+        answer += n_str[i]-'0'  // 각 digit을 꺼냄
+    }
+}
