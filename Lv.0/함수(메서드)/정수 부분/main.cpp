dd67c557676a401940c43c3eb4a913e2936e01f4
Justice <63052097+juijeong8324@users.noreply.github.com>
2023-05-05 13:52:34 +0900

    Create main.cpp

---
+++ Lv.0/함수(메서드)/정수 부분/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = 0;
+    answer = (int)flo;
+    return answer;
+}
+
+/* 다른 사람의 풀이 */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = flo; 
+    return answer;
+}
