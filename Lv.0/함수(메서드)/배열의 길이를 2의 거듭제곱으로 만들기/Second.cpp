823c9fff1322a1720950bfc8930f7df70b2c3d85
Justice <63052097+juijeong8324@users.noreply.github.com>
2023-05-05 19:44:14 +0900

    Create Second.cpp

---
+++ Lv.0/함수(메서드)/배열의 길이를 2의 거듭제곱으로 만들기/Second.cpp
@@ -0,0 +1,17 @@
+// 프로그래머스 '한창균'님의 풀이 과정
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    int n = arr.size();
+    int dst = pow(2, ceil(log2(n))); // 예를 들어 n=6이면 (2 < 루트 6 < 3) 이므로! 
+
+    for(int t = 0; t < dst - n; ++t)
+    {
+        arr.push_back(0);
+    }
+
+    return arr;
+}
